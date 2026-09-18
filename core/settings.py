6837dca91eb1a515202dff6a082850be111b3ca5
it config --global user.name <princegdas193@gmail.com>
2026-09-18 09:34:19 +0530

    hi

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
 AUTH_USER_MODEL = 'users.User'
 
 # Application definition
